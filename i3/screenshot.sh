9069d1440d1fc49ce351f254413270a694e5a6e6
p.kramer <p.kramer@tu-bs.de>
2021-11-08 21:17:20 +0100

    Update screenshot script

--- i3/screenshot.sh
+++ i3/screenshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-icon="/usr/share/icons/HighContrast/256x256/apps/applets-screenshooter.png"
+icon="/usr/share/icons/HighContrast/48x48/apps/applets-screenshooter.png"
 
 directory="/home/$USER/Pictures/"
 filename=$(date +"%b%d-%Y-%H%M%S").png
